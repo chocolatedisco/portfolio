f95d024de71969f6dc356fcc61ec5c31245969b3
chocolatedisco <yuichiro.hack@gmail.com>
2020-08-04 14:09:56 +0900

    add test header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,18 @@
 <template>
   <div>
+    <Header /> 
     <Nuxt />
   </div>
 </template>
+
+<script>
+	import Header from "@/components/header.vue"; // headerをインポート
+	export default {
+		components: {
+			Header,
+		}
+	};
+</script>
 
 <style>
 html {
